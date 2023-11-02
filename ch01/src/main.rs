205220cecface5965d9f697a7996b7c1496f12b4
zwpdbh <hyperion_z@outlook.com>
2023-11-02 16:59:37 +0800

    > debug error

--- ch01/src/main.rs
+++ ch01/src/main.rs
@@ -3,9 +3,26 @@
 #![no_main]
 
 mod lang_items;
+mod sbi;
 
-// fn main() {
-//     //println!("Hello, world!");
-// }
+#[macro_use]
+mod console;
+
 use core::arch::global_asm;
 global_asm!(include_str!("entry.asm"));
+
+#[no_mangle]
+pub fn rust_main() -> ! {
+    clear_bass();
+    println!("Hello, world!");
+    panic!("Shutdown machine!");
+}
+
+fn clear_bass() {
+    extern "C" {
+        fn sbss();
+        fn ebss();
+    }
+
+    (sbss as usize..ebss as usize).for_each(|a| unsafe { (a as *mut u8).write_volatile(0) });
+}
