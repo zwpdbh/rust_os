e195c6e15dd65f54b16eb028b108f1f1b892f433
zwpdbh <hyperion_z@outlook.com>
2023-11-03 12:30:47 +0800

    < review ch01

--- ch01/src/entry.asm
+++ ch01/src/entry.asm
@@ -7,7 +7,7 @@ _start:
   .section .bss.stack
   .globl boot_stack
 
-boot_stack:
+boot_stack_lower_bound:
   .space 4096 * 16
   .globl boot_stack_top
 boot_stack_top:
--- ch01/src/main.rs
+++ ch01/src/main.rs
@@ -1,3 +1,12 @@
+//! The main module and entrypoint
+//!
+//! The operating system and app also starts in this module. Kernel code starts
+//! executing from `entry.asm`, after which [`rust_main()`] is called to
+//! initialize various pieces of functionality [`clear_bss()`]. (See its source code for
+//! details.)
+//!
+//! We then call [`println!`] to display `Hello, world!`.
+
 #![deny(missing_docs)]
 #![deny(warnings)]
 #![no_std]
